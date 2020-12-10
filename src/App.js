98dec37c01e0370d4a3a81ac15d93151aba6c492
Emylly Sa <201810189@acad.unijuazeiro.edu.br>
2020-12-10 17:24:52 -0300

    add build project to ci workflow

--- src/App.js
+++ src/App.js
@@ -15,9 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Conheça a Unijuazeiro
+          Conheça a Unijuazeirio
         </a>
-      </header>
     </div>
   );
 }
